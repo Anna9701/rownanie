5026f0babae5506d3cc14185bd8abc0bbda35aeb
Anna9701 <anna130197@gmail.com>
2017-03-16 10:08:30 +0100

    poprawiona kompozycja

--- rownanie_kwadratowe.java
+++ rownanie_kwadratowe.java
@@ -1,26 +1,19 @@
-class rownanie_kwadratowe{
+public class rownanie_kwadratowe {
+	private
 		double a, b, c;
 		double result1, result2;
 		double delta; 
-		
-		void printResults(){
-			if(delta < 0)
-				System.out.println("No results");
-			else if(delta == 0)
-				System.out.printf("x = %f\n", result1);
-			else
-				System.out.printf("x1 = %f \nx2 = %f \n", result1, result2);
-		}
+	public
 		rownanie_kwadratowe(double x, double y, double z){
 			a = x;
 			b = y;
 			c = z;
 		}
-
+	
 		rownanie_kwadratowe(String[] args){
-			int size = 3;
+			final int size = 3;
 			double tab[] = new double[size];
-			if(args.length < 3){
+			if(args.length < size){
 				System.out.println("Too few arguments!");
 				System.exit(1);
 			}
@@ -35,16 +28,9 @@ class rownanie_kwadratowe{
 			b = tab[1]; 
 			c = tab[2];
 		}
-		public static void main(String []args){
-			rownanie_kwadratowe rownanie1 = new rownanie_kwadratowe(args);
-			rownanie1.oblicz();
-			rownanie1.printResults();
-			return;
-
-		}
+		
 		void oblicz(){
-			delta = b*b - (4*a*c);
-			delta = Math.sqrt(delta); 
+			delta = Math.sqrt(b*b - (4*a*c));
 						
 			if(delta == 0)
 				result1 = -b/(2*a);
@@ -52,8 +38,23 @@ class rownanie_kwadratowe{
 				result1 = (-b-delta)/(2*a);
 				result2 = (-b+delta)/(2*a);
 			}
-		}		
-}
+		}	
+		
+		void printResults(){
+			if(delta < 0)
+				System.out.println("No results");
+			else if(delta == 0)
+				System.out.printf("x = %f\n", result1);
+			else
+				System.out.printf("x1 = %f \nx2 = %f \n", result1, result2);
+		}
+		
+		public static void main(String []args){
+			rownanie_kwadratowe rownanie1 = new rownanie_kwadratowe(args);
+			rownanie1.oblicz();
+			rownanie1.printResults();
+			
+			return;
+		}
 
-	
-					
+}
