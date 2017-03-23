220b8dc2c1c2e5e4f4cf7bfa2419eb88cfbb3932
Anna9701 <anna130197@gmail.com>
2017-03-23 09:59:26 +0100

    ..

--- rownanie_kwadratowe.java
+++ rownanie_kwadratowe.java
@@ -1,60 +1,66 @@
-public class rownanie_kwadratowe {
-	private
-		double a, b, c;
-		double result1, result2;
-		double delta; 
-	public
-		rownanie_kwadratowe(double x, double y, double z){
-			a = x;
-			b = y;
-			c = z;
-		}
+class Rownanie{
+	private	double a, b, c, result1, result2, delta;
+	private static final int SIZE = 3;
 	
-		rownanie_kwadratowe(String[] args){
-			final int size = 3;
-			double tab[] = new double[size];
-			if(args.length < size){
-				System.out.println("Too few arguments!");
-				System.exit(1);
-			}
-			try{
-				for(int i=0; i < size; i++)
-					tab[i] = Double.parseDouble(args[i]);
-			}catch(NumberFormatException error1){
-				System.out.println("Wrong format of arguments");
-				System.exit(2);
-			}
-			a = tab[0];
-			b = tab[1]; 
-			c = tab[2];
+	Rownanie(double x, double y, double z){
+		if((a = x) == 0){
+			System.out.println("Argument a musn't be 0!");
+			System.exit(3);
 		}
+		b = y;
+		c = z;
+	}
+	
+	Rownanie(String[] args){
+		double tab[] = new double[SIZE];
+		if(args.length < SIZE){
+			System.out.println("Too few arguments!");
+			System.exit(1);
+		}
+		try{
+			for(int i=0; i < SIZE; i++)
+				tab[i] = Double.parseDouble(args[i]);
+		}catch(NumberFormatException error1){
+			System.out.println("Wrong format of arguments");
+			System.exit(2);
+		}
+		if((a = tab[0]) == 0){
+			System.out.println("Argument a musn't be 0!");
+			System.exit(3);
+		}
+		b = tab[1]; 
+		c = tab[2];
+	}
 		
-		void oblicz(){
-			delta = Math.sqrt(b*b - (4*a*c));
+	void oblicz(){
+		delta = Math.sqrt(b*b - (4*a*c));
 						
-			if(delta == 0)
-				result1 = -b/(2*a);
-			else{
-				result1 = (-b-delta)/(2*a);
-				result2 = (-b+delta)/(2*a);
-			}
-		}	
-		
-		void printResults(){
-			if(delta < 0)
-				System.out.println("No results");
-			else if(delta == 0)
-				System.out.printf("x = %f\n", result1);
-			else
-				System.out.printf("x1 = %f \nx2 = %f \n", result1, result2);
+		if(Double.isNaN(delta))
+			result1 = -b/(2*a);
+		else{
+			result1 = (-b-delta)/(2*a);
+			result2 = (-b+delta)/(2*a);
 		}
+	}	
 		
-		public static void main(String []args){
-			rownanie_kwadratowe rownanie1 = new rownanie_kwadratowe(args);
-			rownanie1.oblicz();
-			rownanie1.printResults();
-			
-			return;
-		}
-
+	void printResults(){
+		if(result1 == 0.0D)
+			System.out.println("No results");
+		else if(result2 != 0.0D)
+			System.out.println("x1 = " + result1 + "    x2 = " + result2);
+		else
+			System.out.println("x = " + result1);
+	}		
 }
+
+public class rownanie_kwadratowe{
+	public static void main(String []args){
+		Rownanie rownanie1 = new Rownanie(args);
+		rownanie1.oblicz();
+		rownanie1.printResults();
+		
+		return;
+	}
+}
+
+
