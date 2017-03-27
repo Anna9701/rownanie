e3c0fc5cf631445239ceb92a413e9e43e5e14307
Anna <anna130197@gmail.com>
2017-03-27 15:35:51 +0200

    Add files via upload

--- rownanie_kwadratowe.java
+++ rownanie_kwadratowe.java
@@ -1,30 +1,33 @@
-class Rownanie{
-	private	double a, b, c, result1, result2, delta;
+class Rownanie {
+	private	double a, b, c;
+	private double results[] = new double [0];
 	private static final int SIZE = 3;
 	
-	Rownanie(double x, double y, double z){
-		if((a = x) == 0){
+	/*Rownanie(double x, double y, double z) {
+		if((a = x) == 0) {
 			System.out.println("Argument a musn't be 0!");
 			System.exit(3);
 		}
 		b = y;
 		c = z;
-	}
+	}*/
 	
-	Rownanie(String[] args){
+	Rownanie(String[] arguments) {
 		double tab[] = new double[SIZE];
-		if(args.length < SIZE){
-			System.out.println("Too few arguments!");
+		if(arguments.length != SIZE) {
+			System.out.println("Wrong amount of arguments!");
 			System.exit(1);
 		}
-		try{
-			for(int i=0; i < SIZE; i++)
-				tab[i] = Double.parseDouble(args[i]);
-		}catch(NumberFormatException error1){
+		try {
+			for(int i=0; i < SIZE; i++){
+				tab[i] = Double.parseDouble(arguments[i]);
+			}
+		}catch(NumberFormatException error1) {
 			System.out.println("Wrong format of arguments");
 			System.exit(2);
 		}
-		if((a = tab[0]) == 0){
+		a = tab[0];
+		if(a == 0) {
 			System.out.println("Argument a musn't be 0!");
 			System.exit(3);
 		}
@@ -32,30 +35,35 @@ class Rownanie{
 		c = tab[2];
 	}
 		
-	void oblicz(){
-		delta = b*b - (4*a*c);
+	void oblicz() {
+		double delta = b * b - (4 * a * c);
 		
-		if(delta == 0)
-			result1 = -b/(2*a);
-		else if (delta > 0){
+		if(delta == 0) {
+			results = new double[1];
+			results[0] = -b / (2 * a);
+		}else if (delta > 0) {
 			delta = Math.sqrt(delta);
-			result1 = (-b-delta)/(2*a);
-			result2 = (-b+delta)/(2*a);
+			results = new double[2];
+			results[0] = (-b - delta) / (2 * a);
+			results[1] = (-b + delta) / (2 * a);
 		}
 	}	
 		
-	void printResults(){
-		if(result1 == 0.0D)
+	void printResults() {
+		if(results.length == 1) {
+			System.out.println("x = " + results[0]);
+		}
+		else if(results.length == 2) {
+			System.out.println("x1 = " + results[0] + "    x2 = " + results[1]);
+		}
+		else {
 			System.out.println("No results");
-		else if(result2 != 0.0D)
-			System.out.println("x1 = " + result1 + "    x2 = " + result2);
-		else
-			System.out.println("x = " + result1);
+		}
 	}		
 }
 
-public class rownanie_kwadratowe{
-	public static void main(String []args){
+public class rownanie_kwadratowe {
+	public static void main(String []args) {
 		Rownanie rownanie1 = new Rownanie(args);
 		rownanie1.oblicz();
 		rownanie1.printResults();
